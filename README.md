444afd0f495a41d2a51e3926e1c1f07114e98b4f
Matheus Paulino Ribeiro <mathpaulinor@gmail.com>
2023-02-25 20:29:57 -0300

    feature: Inclusão de rotina para minimizar o eclipse
    - Inclusão de métodos para minimizar o eclipse
    - Inclusão da imagem necessária
    - Atualização do README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sikulix-automations
 Rotinas automatizadas utilizando a API do SikuliX.
+
+## Variáveis de ambiente
+* LOCAL_PROJECT_PATH: Contém o caminho do diretório do projeto.
--- src/main/java/br/com/matheus/sikulix/automations/GmailAutomation.java
+++ src/main/java/br/com/matheus/sikulix/automations/GmailAutomation.java
@@ -1,8 +1,26 @@
 package br.com.matheus.sikulix.automations;
 
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.ImagePath;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+
 public class GmailAutomation {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws FindFailed {
+		GmailAutomation.setImagePath();
+		GmailAutomation.minimize();
+	}
+	
+	private static void setImagePath() {
+		final String LOCAL_PROJECT_PATH = System.getenv("LOCAL_PROJECT_PATH");
+		final String IMAGES_ROOT_PATH = "sikulix-automations\\src\\main\\resources\\sikuli-imgs\\";
+		ImagePath.add(LOCAL_PROJECT_PATH.concat(IMAGES_ROOT_PATH));
+	}
+	
+	private static void minimize() throws FindFailed {	
+		Screen screen = new Screen();
+		Pattern minimize = new Pattern("eclipse\\window-menu.png").targetOffset(-45, 0);
+		screen.click(minimize);
 	}
 }
