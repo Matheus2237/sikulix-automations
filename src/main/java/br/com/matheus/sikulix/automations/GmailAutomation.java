104542df4ba56d20fceee0a1656e2c48b86208e5
Matheus Paulino Ribeiro <mathpaulinor@gmail.com>
2023-03-01 19:15:09 -0300

    feature: Implementando o login no Gmail
    - Implementando a page refente a página de login do Gmail
    - Adição das imagens para o Sikuli
    - Atualização do README

--- src/main/java/br/com/matheus/sikulix/automations/GmailAutomation.java
+++ src/main/java/br/com/matheus/sikulix/automations/GmailAutomation.java
@@ -6,10 +6,14 @@ import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
 
 import br.com.matheus.sikulix.automations.pages.AnonymousOperaPage;
+import br.com.matheus.sikulix.automations.pages.GmailLoginPage;
 import br.com.matheus.sikulix.automations.pages.TaskBarPage;
 
 public class GmailAutomation {
 
+	final static String USER_GMAIL = System.getenv("USER_GMAIL");
+	final static String PASSWORD_GMAIL = System.getenv("PASSWORD_GMAIL");
+	
 	public static void main(String[] args) throws FindFailed {
 		GmailAutomation.setImagePath();
 		GmailAutomation.minimize();
@@ -20,6 +24,10 @@ public class GmailAutomation {
 		
 		AnonymousOperaPage operaPage = new AnonymousOperaPage();
 		operaPage.search("gmail.com");
+		
+		GmailLoginPage gmailLoginPage = new GmailLoginPage();
+		gmailLoginPage.login(USER_GMAIL, PASSWORD_GMAIL);
+		gmailLoginPage.waitForVerification();
 	}
 	
 	private static void setImagePath() {
